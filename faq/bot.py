0a90d89473183ee7b3ff7989837c71453242c1a3
alireza heydari <alireza97hi@gmail.com>
2020-09-11 03:59:34 +0430

    add markup

--- faq/bot.py
+++ faq/bot.py
@@ -30,14 +30,21 @@ def send_help(message):
 @bot.message_handler(commands=['questions'])
 def send_welcome(message):
     questions = QuestionSet.objects.filter(active=True)
+    
 
     if questions.count() == 0:
         bot.reply_to(message, 'سوالی وجود ندارد.')
+    
+    else:
 
-    text = ''
-    for index, question in enumerate(questions):
-        text += str(index+1) + '.' + question.question + '\n'
-    bot.reply_to(message, text)
+        text = ''
+        markup = telebot.types.ReplyKeyboardMarkup()
+
+        for index, question in enumerate(questions):
+            text += str(index+1) + '.' + question.question + '\n'
+            itembtna = telebot.types.KeyboardButton(str(index+1))
+
+        bot.reply_to(message, text, reply_markup=markup)
 
 
 def start_polling():
