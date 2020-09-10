a916defd3f29fec7831fe1ce07794833db134d4b
alireza heydari <alireza97hi@gmail.com>
2020-09-11 03:53:23 +0430

    complete admin

--- faq/admin.py
+++ faq/admin.py
@@ -4,6 +4,7 @@ from .models import QuestionSet
 # Register your models here.
 
 class QuestionSetAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer', 'active', 'rank')
     fields = ('question', 'answer', 'active')
     readonly_fields = ('rank', )
 
