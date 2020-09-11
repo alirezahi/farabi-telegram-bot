182cd68b23fb89248e8fab412a2298f230f67f01
alireza heydari <alireza97hi@gmail.com>
2020-09-11 16:11:03 +0430

    add telegramuser to admin

--- faq/admin.py
+++ faq/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import QuestionSet, Config
+from .models import QuestionSet, TelegramUser, Config
 
 # Register your models here.
 
@@ -12,5 +12,10 @@ class QuestionSetAdmin(admin.ModelAdmin):
 class ConfigAdmin(admin.ModelAdmin):
     list_display = ('name', 'value')
 
+
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('chat_id', )
+
 admin.site.register(QuestionSet, QuestionSetAdmin)
 admin.site.register(Config, ConfigAdmin)
+admin.site.register(TelegramUser, TelegramUserAdmin)
